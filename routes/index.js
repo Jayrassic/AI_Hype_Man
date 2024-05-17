7043bfc26be58023d69974a3785d284bbae6a1e6
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-05-17 17:18:25 -0400

    Changed route to post to allow data transfer from site.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 // var express = require("express");
 import express from "express";
+import { AI } from "../controllers/indexController.js";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("Hello!");
-});
+router.post("/", AI);
 
 export default router;
