3f6117fdd9a8b77fe0e199b71bcfbd7652ad4d88
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-05-19 10:38:19 -0400

    Added api key processing

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 async function main(message) {
   const completion = await openai.chat.completions.create({
