4222141620ce13a548fffedc2da4ee190624a2ea
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-05-21 13:13:10 -0400

    Added validation and changed error reporting

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 import OpenAI from "openai";
+import { body, validationResult } from "express-validator";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,19 +14,34 @@ async function main(message) {
       },
       message,
     ],
-    model: "ft:gpt-3.5-turbo-0125:personal:hypeman:9QRTCvlw",
+    model: "ft:gpt-3.5-turbo-0125:personal:hypeman:9RM7FwbQ",
   });
   return completion.choices[0].message;
 }
+const AI = [
+  body("message").isObject(),
+  body("message.role").isString().trim().escape(),
+  body("message.content").isString().trim().escape(),
 
-async function AI(req, res, next) {
-  const message = req.body;
-  try {
-    const answer = await main(message);
-    res.send(answer);
-  } catch (err) {
-    res.send(err);
-  }
-}
+  async function AI(req, res, next) {
+    console.log(req.body);
+
+    const errors = validationResult(req);
+    const message = req.body.message;
+
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      res.status(400).send({ error: { message: "Form Validation Failure" } });
+    } else {
+      try {
+        const answer = await main(message);
+        res.status(200).send(answer);
+      } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+      }
+    }
+  },
+];
 
 export { AI };
